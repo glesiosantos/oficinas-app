cd70306581be80d4765c118a02aee6702147da90
Glêsio Santos <glesioss@gmail.com>
2025-03-31 10:23:53 -0300

    Organizando a tela de perfil do usuário

--- src/pages/colaborador/ColaboradorPerfilPage.vue
+++ src/pages/colaborador/ColaboradorPerfilPage.vue
@@ -1,5 +1,87 @@
 <template>
-  <q-page>
-    <h1>Perfil do colaborador</h1>
+  <q-page padding>
+    <q-card class="q-pa-md">
+      <q-card-section>
+        <div class="text-h6">Meu Perfil</div>
+      </q-card-section>
+
+      <q-card-section>
+        <q-list bordered separator>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Nome</q-item-label>
+              <q-item-label>{{authStore.auth.nome}}</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Perfil</q-item-label>
+              <q-item-label>{{authStore.auth.perfil}}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-card-section>
+
+      <q-card-section>
+        <span>Alterar a senha de acesso</span>
+        <q-form @submit="changePassword">
+          <div class="row q-col-gutter-sm q-mt-xs">
+            <q-input
+            v-model="form.nova"
+            label="Nova Senha"
+            type="password"
+            class="col-12 col-md-6"
+            outlined
+            lazy-rules
+            :rules="[val => val.length >= 6 || 'A senha deve ter no mínimo 6 caracteres']"
+          />
+
+          <q-input
+            v-model="form.confirmar"
+            label="Confirma senha"
+            type="password"
+            class="col-12 col-md-6"
+            outlined
+            lazy-rules
+            :rules="[val => val.length >= 6 || 'A senha deve ter no mínimo 6 caracteres']"
+          />
+          </div>
+
+          <div class="row q-gutter-sm">
+            <q-btn label="Alterar Senha" type="submit" color="accent" :class="{'full-width': $q.screen.xs}"/>
+            <q-btn label="Cancelar" @click="handleCancelar" size="md" color="red" :class="{'full-width': $q.screen.xs}"/>
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
+
+<script setup>
+import { useAuthStore } from 'src/stores/auth.store'
+import { reactive } from 'vue'
+
+const authStore = useAuthStore()
+
+
+let form = reactive({
+  nova: '',
+  confirmar: ''
+})
+
+const changePassword = async () => {
+
+  if (form.nova !== form.confirmar) {
+    return
+  }
+
+  form = {}
+
+}
+
+const handleCancelar = () => {
+  form = {}
+}
+
+</script>
