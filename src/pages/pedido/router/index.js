a2df19febb716d475bd9d06a9c167410a21464c6
Glêsio Santos <glesioss@gmail.com>
2025-05-05 19:38:04 -0300

    ajuste de verificação do token

--- src/pages/pedido/router/index.js
+++ src/pages/pedido/router/index.js
@@ -5,5 +5,6 @@ const PedidoOficinaListPage = import('../PedidoOficinaListPage.vue')
 export default [
   { path: '/pedidos', name: 'pedidos', component: () =>  PedidoListaPage},
   { path: '/pedidos/criar', name: 'criar-pedido', component: () =>  CriarPedidoPage},
+  { path: '/pedidos/editar/:id', name: 'editar-pedido', component: () =>  CriarPedidoPage},
   { path: '/pedidos/oficina', name: 'naOficina', component: () =>  PedidoOficinaListPage}
 ]
